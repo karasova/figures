fc1c3fbd8101aba145aed228d38437b240903399
karasova <32793285+karasova@users.noreply.github.com>
2019-06-16 16:16:13 +0800

    added poligon

--- main.cpp
+++ main.cpp
@@ -13,23 +13,30 @@ int main()
 	Circle<double>* circle = new Circle<double>(4);
 	Rectangle<double>* rectangle = new Rectangle<double>(4, 9);
 	Triangle<double>* triangle = new Triangle<double>(4, 9, 12);
+	Poligon<double>* poligon = new Poligon<double>();
+	poligon->setDot(3, 9);
+	poligon->setDot(1, 4);
+	poligon->setDot(4, 8);
+	poligon->setDot(6, 6);
+	poligon->setDot(3, 5);
 
 	vector<Figure*>fig;
 	fig.push_back(circle);
 	fig.push_back(rectangle);
 	fig.push_back(triangle);
+	fig.push_back(poligon);
 
 	double area = 0, per = 0;
-	cout << "Ôèãóðû:" << endl;
+	cout << "Ð¤Ð¸Ð³ÑƒÑ€Ñ‹:" << endl;
 	for (int i = 0; i < fig.size(); i++) {
-		cout << fig[i]->name() << ": ïëîùàäü = " << fig[i]->area() << ", ïåðèìåòð = " << fig[i]->per() << endl;
+		cout << fig[i]->name() << ": Ð¿Ð»Ð¾Ñ‰Ð°Ð´ÑŒ = " << fig[i]->area() << ", Ð¿ÐµÑ€Ð¸Ð¼ÐµÑ‚Ñ€ = " << fig[i]->per() << endl;
 		area += fig[i]->area();
 		per += fig[i]->per();
 	}
 	cout << endl;
 
-	cout << "Îáùàÿ ïëîùàäü: " << area << endl;
-	cout << "Îáùèé ïåðèìåòð: " << per << endl;
+	cout << "ÐžÐ±Ñ‰Ð°Ñ Ð¿Ð»Ð¾Ñ‰Ð°Ð´ÑŒ: " << area << endl;
+	cout << "ÐžÐ±Ñ‰Ð¸Ð¹ Ð¿ÐµÑ€Ð¸Ð¼ÐµÑ‚Ñ€: " << per << endl;
 
 	system("pause");
 	return 0;
